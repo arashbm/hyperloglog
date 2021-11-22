9e4ce468ed8ae04cb18418fe339a8a4f4ed4afad
Arash Badie Modiri <arashbm@gmail.com>
2021-11-22 17:34:52 +0200

    use better guards for MurmurHash3.h

--- dep/MurmurHash3/include/MurmurHash3.h
+++ dep/MurmurHash3/include/MurmurHash3.h
@@ -1,10 +1,10 @@
+#ifndef _MURMURHASH3_H_
+#define _MURMURHASH3_H_
+
 namespace murmur3 {
 //-----------------------------------------------------------------------------
 // MurmurHash3 was written by Austin Appleby, and is placed in the public
 // domain. The author hereby disclaims copyright to this source code.
-
-#ifndef _MURMURHASH3_H_
-#define _MURMURHASH3_H_
 
 //-----------------------------------------------------------------------------
 // Platform-specific functions and macros
@@ -35,7 +35,6 @@ void MurmurHash3_x64_128 ( const void * key, int len, uint32_t seed, void * out 
 
 //-----------------------------------------------------------------------------
 
-#endif // _MURMURHASH3_H_
-
 #include "../src/MurmurHash3.cpp"
 }
+#endif // _MURMURHASH3_H_
